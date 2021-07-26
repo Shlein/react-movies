32a364e9b019211de142c4a8abb288471edf5b8d
shlein <optime15022013@mail.ru>
2021-07-27 03:04:45 +0800

    project complete

---
+++ react-testing/src/App.js
@@ -0,0 +1,15 @@
+import React, {useState, useReducer} from "react";
+// import Clicker from "./components/Clicker";
+import Timer from "./components/Timer";
+// import Counter from "./components/Counter(reducer)";
+
+function App() {
+
+  return (
+    <div>
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
